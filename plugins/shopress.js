46fb9f707b5c5ce23e09860982a8fc4998e3dfeb
Claudio <claudio.skdf@gmail.com>
2020-07-08 20:07:47 -0400

    fix bug image

--- plugins/shopress.js
+++ plugins/shopress.js
@@ -1,13 +1,13 @@
 import Vue from 'vue';
 
 Vue.prototype.$resizeImage = (url = "", width = 100, height = 100, quality = 80) => {
-    const baseURL = process.env.NODE_ENV !== 'production' ? 'http://localhost:2616' : 'https://api.shopress.com';
+    const baseURL = process.env.NODE_ENV !== 'production' ? process.env.API_URL_DEV : process.env.API_URL_PRODUCTION;
     if(/svg/i.test(url.split('.').pop())) return url
     else return `${baseURL}/jimp/resize?url=${url}&w=${width}&h=${height}&q=${quality}`
 };
 
 Vue.prototype.$scaleImage = (url = "", width = 100, quality = 80) => {
-    const baseURL = process.env.NODE_ENV !== 'production' ? 'http://localhost:2616' : 'https://api.shopress.com';
+    const baseURL = process.env.NODE_ENV !== 'production' ? process.env.API_URL_DEV : process.env.API_URL_PRODUCTION;
     if(/svg/i.test(url.split('.').pop())) return url
     else return `${baseURL}/jimp/scale?url=${url}&w=${width}&q=${quality}`
 };
